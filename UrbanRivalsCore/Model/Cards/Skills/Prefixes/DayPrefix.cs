a7edaa6b150d4f1bd756cb20765340874d94c286
Guillermo Mestre <warcraker@gmail.com>
2018-12-25 12:15:03 +0100

    Add day and night prefixes

---
+++ UrbanRivalsCore/Model/Cards/Skills/Prefixes/DayPrefix.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UrbanRivalsCore.Model.Cards.Skills.Prefixes
+{
+    public class DayPrefix : Prefix
+    {
+        private static readonly Regex PRV_DAY_REGEX = new Regex("^Day: ");
+
+        public override bool isMatch(string text)
+        {
+            const string PRV_DAY_PREFIX = "Day: ";
+
+            return text.StartsWith(PRV_DAY_PREFIX);
+        }
+
+        public override string removePrefixFromText(string text)
+        {
+            return PRV_DAY_REGEX.Replace(text, "");
+        }
+    }
+}
---
+++ UrbanRivalsCore/Model/Cards/Skills/Prefixes/NightPrefix.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UrbanRivalsCore.Model.Cards.Skills.Prefixes
+{
+    public class NightPrefix : Prefix
+    {
+        private static readonly Regex PRV_NIGHT_REGEX = new Regex("^Night: ");
+
+        public override bool isMatch(string text)
+        {
+            const string PRV_NIGHT_PREFIX = "Night: ";
+
+            return text.StartsWith(PRV_NIGHT_PREFIX);
+        }
+
+        public override string removePrefixFromText(string text)
+        {
+            return PRV_NIGHT_REGEX.Replace(text, "");
+        }
+    }
+}
