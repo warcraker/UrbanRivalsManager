4ded27dbe7ba1c7c1d57afd48e5fa74fa030cfd5
Guillermo Mestre <warcraker@gmail.com>
2018-12-26 20:18:10 +0100

    Add Cancel Leader Suffix

---
+++ UrbanRivalsCore/Model/Cards/Skills/Suffixes/CancelLeaderSuffix.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using UrbanRivalsCore.Model.Cards.Skills.SuffixParsers;
+
+namespace UrbanRivalsCore.Model.Cards.Skills.Suffixes
+{
+    public class CancelLeaderSuffix : Suffix
+    {
+        private readonly static PlainSuffixParser PRV_PARSER;
+        private readonly static string PRV_TEXT_REPRESENTATION;
+
+        static CancelLeaderSuffix()
+        {
+            Regex regex = new Regex(@"^Cancel Leader$");
+
+            PRV_PARSER = new PlainSuffixParser(regex, new CancelLeaderSuffix());
+            PRV_TEXT_REPRESENTATION = Properties.GameStrings.skill_suffix_cancel_leader;
+        }
+
+        public CancelLeaderSuffix() : base()
+        {
+            ;
+        }
+
+        public static PlainSuffixParser getParser()
+        {
+            return PRV_PARSER;
+        }
+
+        public override string ToString()
+        {
+            return PRV_TEXT_REPRESENTATION;
+        }
+    }
+}
+
