ca53ddd03bbaba3f1bbdbd9d69340411cea1cd56
Guillermo Mestre <warcraker@gmail.com>
2019-10-08 21:34:04 +0200

    Add PKs to Tables

--- Warcraker.UrbanRivals.DataRepository/DataModels/SkillData.cs
+++ Warcraker.UrbanRivals.DataRepository/DataModels/SkillData.cs
@@ -1,18 +1,21 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Warcraker.UrbanRivals.DataRepository.DataModels
 {
+    [Table(nameof(SkillData))]
     public class SkillData
     {
         [PrimaryKey]
+        [Column(nameof(Hash))]
         public int Hash { get; set; }
 
+        [Column(nameof(SuffixClassName))]
         public string SuffixClassName { get; set; }
+        [Column(nameof(PrefixesClassNames))]
         public string[] PrefixesClassNames { get; set; }
+        [Column(nameof(X))]
         public int X { get; set; }
+        [Column(nameof(Y))]
         public int Y { get; set; }
     }
 }
