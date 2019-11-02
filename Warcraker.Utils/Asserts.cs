41624d818fc09353548eea8ea222e6257f090a3e
Guillermo Mestre <warcraker@gmail.com>
2019-11-02 12:56:28 +0100

    Disable asserts on Release

--- Warcraker.Utils/Asserts.cs
+++ Warcraker.Utils/Asserts.cs
@@ -14,15 +14,19 @@ namespace Warcraker.Utils
 
         public static void Check(bool condition, string message)
         {
+#if DEBUG
             if (condition == false)
             {
                 throw new AssertException(message);
             }
+#endif
         }
 
         public static void Fail(string message)
         {
+#if DEBUG
             throw new AssertException(message);
+#endif
         }
     }
 
@@ -30,39 +34,49 @@ namespace Warcraker.Utils
     {
         public static void Fail(string message, string paramName)
         {
+#if DEBUG
             throw new ArgumentException(paramName, message);
+#endif
         }
 
         public static void Check(bool condition, string message, string paramName)
         {
+#if DEBUG
             if (condition == false)
             {
                 throw new ArgumentException(paramName, message);
             }
+#endif
         }
 
         public static void CheckIntegerRange(bool condition, string message, int paramValue, string paramName)
         {
+#if DEBUG
             if (condition == false)
             {
                 throw new ArgumentOutOfRangeException(paramName, paramValue, message);
             }
+#endif
         }
 
         public static void CheckIsNotNull<T>(T paramValue, string paramName)
         {
+#if DEBUG
             if (paramValue == null)
             {
                 throw new ArgumentNullException(paramName);
             }
+#endif
         }
 
         public static void StringIsFilled(string paramValue, string paramName)
         {
+#if DEBUG
             if (String.IsNullOrWhiteSpace(paramValue) == true)
             {
                 throw new ArgumentNullException("The string cannot be null nor whitespace", paramName);
             }
+#endif
         }
     }
 }
