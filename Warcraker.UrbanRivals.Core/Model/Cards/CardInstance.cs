04b4e2675234c523d9624758614c9d55f76abd8c
Guillermo Mestre <warcraker@gmail.com>
2021-03-27 16:29:16 +0100

    Added CardInstance

---
+++ Warcraker.UrbanRivals.Core/Model/Cards/CardInstance.cs
@@ -0,0 +1,64 @@
+﻿using Warcraker.UrbanRivals.Core.Model.Cards.Skills;
+using Warcraker.Utils;
+
+namespace Warcraker.UrbanRivals.Core.Model.Cards
+{
+    public class CardInstance
+    {
+        public int Level { get; }
+        public int Experience { get; }
+
+        public int Id { get; }
+        public string Name { get; }
+        public Clan Clan { get; }
+
+        public int Power { get; }
+        public int Damage { get; }
+        public Skill Ability { get; }
+
+        public CardInstance(CardDefinition definition, int level, int experience)
+        {
+            // TODO asserts
+
+            this.Level = level;
+            this.Experience = experience;
+
+            this.Id = definition.GameId;
+            this.Name = definition.Name;
+            this.Clan = definition.Clan;
+            this.Power = definition.Stats[Level].Power;
+            this.Damage = definition.Stats[Level].Damage;
+            
+            if (definition.Ability.Equals(NoAbility.INSTANCE))
+            {
+                this.Ability = NoAbility.INSTANCE;
+            }
+            else if (level >= definition.AbilityUnlockLevel)
+            {
+                this.Ability = definition.Ability;
+            }
+            else
+            {
+                switch (definition.AbilityUnlockLevel)
+                {
+                    case 2:
+                        this.Ability = LockedSkill.UNLOCKED_AT_LEVEL_2;
+                        break;
+                    case 3:
+                        this.Ability = LockedSkill.UNLOCKED_AT_LEVEL_3;
+                        break;
+                    case 4:
+                        this.Ability = LockedSkill.UNLOCKED_AT_LEVEL_4;
+                        break;
+                    case 5:
+                        this.Ability = LockedSkill.UNLOCKED_AT_LEVEL_5;
+                        break;
+                    default:
+                        this.Ability = UnknownSkill.INSTANCE;
+                        Asserts.Fail("Invalid skill");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Warcraker.UrbanRivals.Core/Model/Cards/Skills/LockedSkill.cs
@@ -0,0 +1,36 @@
+﻿using Warcraker.UrbanRivals.Core.Model.Cards.Skills.Suffixes;
+using Warcraker.UrbanRivals.Core.Model.Cards.Skills.Suffixes.Placeholders;
+using Warcraker.Utils;
+
+namespace Warcraker.UrbanRivals.Core.Model.Cards.Skills
+{
+    public class LockedSkill : Skill
+    {
+        public static readonly Skill UNLOCKED_AT_LEVEL_2 = new LockedSkill(2);
+        public static readonly Skill UNLOCKED_AT_LEVEL_3 = new LockedSkill(3);
+        public static readonly Skill UNLOCKED_AT_LEVEL_4 = new LockedSkill(4);
+        public static readonly Skill UNLOCKED_AT_LEVEL_5 = new LockedSkill(5);
+        private LockedSkill(int unlockLevel) : base(new Prefix[] { }, NoAbilitySuffix.INSTANCE)
+        {
+            switch (unlockLevel)
+            {
+                case 2:
+                    this.Suffix = UnlockedAtLevelXSuffix.UNLOCKED_AT_LEVEL_2;
+                    break;
+                case 3:
+                    this.Suffix = UnlockedAtLevelXSuffix.UNLOCKED_AT_LEVEL_3;
+                    break;
+                case 4:
+                    this.Suffix = UnlockedAtLevelXSuffix.UNLOCKED_AT_LEVEL_4;
+                    break;
+                case 5:
+                    this.Suffix = UnlockedAtLevelXSuffix.UNLOCKED_AT_LEVEL_5;
+                    break;
+                default:
+                    Asserts.Fail($"Invalid {nameof(unlockLevel)}: {unlockLevel}");
+                    this.Suffix = UnknownSuffix.INSTANCE;
+                    break;
+            }
+        }
+    }
+}
