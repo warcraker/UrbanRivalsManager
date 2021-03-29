dabd83548db963416179d515b52cc5f852a0d90f
Guillermo Mestre <warcraker@gmail.com>
2021-03-29 20:27:08 +0200

    Move viewmodels to its own project

---
+++ Warcraker.UrbanRivals.URManager.ViewModels/Language/LanguageConfigurationVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Warcraker.UrbanRivals.URManager.Configuration;
+using Warcraker.Utils;
+
+namespace Warcraker.UrbanRivals.URManager.ViewModels.Language
+{
+    public class LanguageConfigurationVM
+    {
+        private readonly ILogger<LanguageConfigurationVM> log;
+        private readonly URConfigurationManager config;
+
+        public LanguageConfigurationVM(ILogger<LanguageConfigurationVM> log, URConfigurationManager config)
+        {
+            AssertArgument.CheckIsNotNull(log, nameof(log));
+            AssertArgument.CheckIsNotNull(config, nameof(config));
+
+            this.log = log;
+            this.config = config;
+        }
+
+        public void SetLanguage(string language)
+        {
+            switch (language)
+            {
+                case SupportedLanguages.ENGLISH:
+                case SupportedLanguages.SPANISH:
+                    log.LogDebug("Start application with language {language}", language);
+                    SetApplicationLanguage(language);
+                    break;
+                default:
+                    log.LogWarning("Invalid language on startup [{language}]. Using English per default", language);
+                    Asserts.Fail($"Invalid language on startup [{language}]");
+                    SetApplicationLanguage(SupportedLanguages.ENGLISH);
+                    break;
+            }
+        }
+
+        private void SetApplicationLanguage(string language)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(language);
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(language);
+        }
+    }
+}
---
+++ Warcraker.UrbanRivals.URManager.ViewModels/Startup/StartupVM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Warcraker.UrbanRivals.URManager.Configuration;
+using Warcraker.UrbanRivals.URManager.ViewModels.Language;
+using Warcraker.Utils;
+
+namespace Warcraker.UrbanRivals.URManager.ViewModels.Startup
+{
+    public class StartupVM
+    {
+        private readonly ILogger<StartupVM> log;
+        private readonly URConfigurationManager config;
+
+        public StartupVM(ILogger<StartupVM> log, URConfigurationManager config)
+        {
+            AssertArgument.CheckIsNotNull(log, nameof(log));
+            AssertArgument.CheckIsNotNull(config, nameof(config));
+
+            this.log = log;
+            this.config = config;
+        }
+
+        public void OnApplicationStart()
+        {
+            log.LogInformation("Starting application...");
+
+            if (config.IsFirstRun)
+            {
+                ExecuteFirstTimeConfiguration();
+                config.IsFirstRun = false;
+            }
+
+            throw new NotImplementedException();
+        }
+        private void ExecuteFirstTimeConfiguration()
+        {
+            log.LogInformation("Executing first time configuration...");
+            
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
