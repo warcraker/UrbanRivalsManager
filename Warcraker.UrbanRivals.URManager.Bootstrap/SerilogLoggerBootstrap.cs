581b32c614789bb69f498cdc811a56da13d740f0
Guillermo Mestre <warcraker@gmail.com>
2021-04-05 12:36:56 +0200

    Add Serilog support

---
+++ Warcraker.UrbanRivals.URManager.Bootstrap/SerilogLoggerBootstrap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac;
+using Serilog;
+using Serilog.Extensions.Autofac.DependencyInjection;
+
+namespace Warcraker.UrbanRivals.URManager.Bootstrap
+{
+    public static class SerilogLoggerBootstrap
+    {
+        public static ContainerBuilder BuildTextFileLogger(this ContainerBuilder builder)
+        {
+            DateTime today = DateTime.Today;
+
+            LoggerConfiguration config = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File($@"logs\{today.Year}\{today.Month}\log.txt", rollingInterval: RollingInterval.Day);
+
+            builder.RegisterSerilog(config);
+
+            return builder;
+        }
+    }
+}
