fc8cc4d3eb947ef96f40c89fec5505c9cb7f3150
Guillermo Mestre <warcraker@gmail.com>
2021-12-05 19:28:56 -0800

    Add interfaces project

---
+++ Warcraker.UrbanRivals.Interfaces/ILanguageVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Warcraker.UrbanRivals.Interfaces
+{
+    public interface ILanguageVM
+    {
+        string GetLanguage();
+        void SetLanguage(string language);
+    }
+}
---
+++ Warcraker.UrbanRivals.Interfaces/ISettingsManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Warcraker.UrbanRivals.Interfaces
+{
+    public interface ISettingsManager
+    {
+        string AccessKey { get; set; }
+        string AccessSecret { get; set; }
+        string ConsumerKey { get; set; }
+        string ConsumerSecret { get; set; }
+        string Language { get; set; }
+    }
+}
